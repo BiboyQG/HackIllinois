2b17d0d45ba8399992e23db4e8d87d0c9dcb6095
Banghao <banghao_chi@163.com>
2024-09-17 13:18:30 -0500

    :rocket: Fixed typo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
     return (
         <footer className="mt-20 py-4">
             <div className="flex justify-center">
-                <a href="https://hype.hackillinois.org/" target="_blank" rel="noopener noreferrer">x
+                <a href="https://hype.hackillinois.org/" target="_blank" rel="noopener noreferrer">
                     <img
                         src="https://2020.hackillinois.org/static/media/logo.63edd138.svg"
                         alt="HackIllinois Logo"
